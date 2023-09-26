29fc26945a9ff3ef9161c19d6db49c191905129b
Lucie Gobart <lucie.gobart@epfedu.fr>
2023-09-26 16:49:21 +0200

    TP0

---
+++ TP0_GOBART/Calculator/src/calculator/Calculator.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lucie, Présentation de java, 26/09/2023
+ */
+public class Calculator {
+
+    /**
+     *
+     * @param args the command line arguments
+     *
+     */
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        System.out.println("Please enter the operator\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer un nombre :");
+        int operateur = sc.nextInt();
+        while (operateur > 5 || operateur < 0) {
+            System.out.println("ERREUR La valeur n'est pas comprise entre 0 et 5");
+            System.out.println("\n Entrer un nombre :");
+            operateur = sc.nextInt();
+
+        }
+
+    }
+    
+
