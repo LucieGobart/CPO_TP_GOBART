e7a1ebf0eb2bd20e0f7437c6c3c0fb4290e6c92e
Lucie Gobart <lucie.gobart@epfedu.fr>
2023-10-13 17:55:18 +0200

    TP2 BouteilleBiere

--- TP2_GOBART/TP2_Bieres_Gobart/src/tp2_bieres_gobart/BouteilleBiere.java
+++ TP2_GOBART/TP2_Bieres_Gobart/src/tp2_bieres_gobart/BouteilleBiere.java
@@ -6,21 +6,46 @@ package tp2_bieres_gobart;
 
 /**
  *
- * @author Lucie
+ * @author Lucie, TP2 Bieres, 13/10/2023
  */
 public class BouteilleBiere {
-
+ 
     String nom;
-    double degreAlcool;
+    float degreAlcool;
     String brasserie;
     boolean ouverte;
 
-    public BouteilleBiere(String unNom, double unDegre,
-            String uneBrasserie) {
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degres) \nBrasserie : " + brasserie);
+    }
+ 
+    public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
         nom = unNom;
         degreAlcool = unDegre;
         brasserie = uneBrasserie;
         ouverte = false;
 
+    }
+
+    public void Décapsuler() {
+        if (ouverte == false) {
+            ouverte = true;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degres) Ouverte ? ";
+        if (ouverte == true) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
     }
 }
