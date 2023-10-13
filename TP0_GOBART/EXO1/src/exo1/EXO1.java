576b1610aa48e6cc82a36e8b92fc82ffeec08086
Lucie Gobart <lucie.gobart@epfedu.fr>
2023-10-13 18:18:58 +0200

    EXO1

---
+++ TP0_GOBART/EXO1/src/exo1/EXO1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lucie Gobart, Présentation de Netbeans, 26/09/2023
+ */
+public class EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+
+}
